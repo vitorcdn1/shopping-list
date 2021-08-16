3559d063b5d6b543339f0ba3bcd8e1c99279221f
vitorcdn1 <vitorcdn11@gmail.com>
2021-08-16 14:57:13 -0300

    Header

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header className="header-container">
+            <div className="header">
+                <h1 className="header-title">Shopping List</h1>
+                <button className="button-show-modal button-restore">+</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
